6f8c0e0ea41bb756ebc136b2046efdc8f51f13e7
Shashank924 <shashankshekhar955@gmail.com>
2025-02-03 11:55:20 +0530

    Basic Setup Completed

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { PORT } = require('./config/serverConfig');
+
+const app = express();
+
+const setupAndStartServer = () => {
+
+    app.listen(PORT , () => {
+        
+        console.log(`Server started at Port ${PORT}`);
+    })
+}
+
+setupAndStartServer();
